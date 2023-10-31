79801ca84205ebce92043c27baa86ba71cddacab
Feruza <111288872+urinbayeeva@users.noreply.github.com>
2023-10-31 18:42:23 +0500

    Update mycard_page.dart

--- lib/pages/mycard_page.dart
+++ lib/pages/mycard_page.dart
@@ -104,14 +104,15 @@ class MyCard extends StatelessWidget {
                   GestureDetector(
                     onTap: (){
                       debugPrint("Tap");
-                      Navigator.pushNamed(context, Wallet.route);
+                      Navigator.pushReplacementNamed(context, HomePage.route);
+          
                     },
                     child:  const Icon(Icons.wallet, size: 25, color: Colors.white,),
                   ),
                   GestureDetector(
                     onTap: (){
                       debugPrint("Tap");
-                      Navigator.pushNamed(context, Notifications.route);
+                      Navigator.pushReplacementNamed(context, HomePage.route);
                     },
                     child:  const Icon(Icons.notifications_active_sharp, size: 25, color: Colors.white,),
                   ),
